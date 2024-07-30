e4b4aad26880001ba4e7b7aad9a209c8ab3ebe87
Samoilov2004 <samoilov.ma@phystech.edu>
2024-07-30 20:50:36 +0300

    rewrite FIBD

--- Bioinformatics_Stronghold/FIBD/FIBD.py
+++ Bioinformatics_Stronghold/FIBD/FIBD.py
@@ -1,9 +1,24 @@
 file_path = './FIBD_input.txt'
 
 with open(file_path, 'r') as f:
-    m, n = list(map(int, f.readline().strip().split()))
+    n, m = list(map(int, f.readline().strip().split()))
 
-#code would be later...
+
+def rabbit_pairs(n, m):
+	#rabbits stand in line of death in this array and move with time
+    R = [0] * m
+    R[0] = 1
+    for month in range(2, n + 1):
+        newborns = sum(R[1:])
+        
+        for i in range(m - 1, 0, -1):
+            R[i] = R[i - 1]
+        R[0] = newborns
+    total_rabbits = sum(R)
+    return total_rabbits
+
+result = rabbit_pairs(n, m)
+print(result)  
 
 with open('./FIBD_output.txt', 'w') as f:
     f.write(str(result))
