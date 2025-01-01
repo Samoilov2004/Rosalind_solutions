726be3561e241936a184f4f2e6927884bf08d96a
Mihail Samoilov <114419554+Samoilov2004@users.noreply.github.com>
2025-01-02 02:12:16 +0300

    Update INI6.py
    Change usual dict to defaultdict

--- Python_Village/INI6.py
+++ Python_Village/INI6.py
@@ -1,13 +1,12 @@
+from collections import defaultdict
+
 file_path = '' #type your path here
 
-d = dict()
+d = defaultdict(int)
 with open(file_path, 'r') as f:
     line = f.readline().strip().split() #would be only 1 line
     for word in line:
-    	if word in d:
-    		d[word] += 1
-    	else:
-    		d[word] = 1
+    	d[word] += 1
 
 sorted_dict = dict(sorted(d.items()))
 
